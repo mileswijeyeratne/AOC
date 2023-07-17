76ea41c740a497bd7560cac7f9de768ad95f8348
M.W <105752803+troubledprogrammer@users.noreply.github.com>
2023-07-17 21:24:36 +0100

    Solutions to 2021 day 10

--- src/_2021/Day10.py
+++ src/_2021/Day10.py
@@ -1,18 +1,82 @@
+from statistics import median
+
 def _parse_data(data):
-    return data
+    return data.split("\n")
 
 
 def solvePartA(data):
     data = _parse_data(data)
-    return
+    openers = ["(", "[", "{", "<"]
+    closers = [")", "]", "}", ">"]
+    scores = {")": 3, "]": 57, "}": 1197, ">": 25137}
+    res = 0
+    for line in data:
+        bracestack = []
+        for char in line:
+            if char in openers:
+                bracestack.append(char)
+            else:
+                if not openers.index(bracestack.pop()) == closers.index(char):
+                    res += scores[char]
+                    break
+    return res
+
+
+def _remove_corrupted(data):
+    openers = ["(", "[", "{", "<"]
+    closers = [")", "]", "}", ">"]
+    res = []
+    for line in data:
+        bracestack = []
+        corrupted = False
+        for char in line:
+            if char in openers:
+                bracestack.append(char)
+            else:
+                if not openers.index(bracestack.pop()) == closers.index(char):
+                    corrupted = True
+                    break
+        if not corrupted:
+            res.append(line)
+    return res
+
+
+def _score(braces):
+    scores = {"(": 1, "[": 2, "{": 3, "<": 4}
+    res = 0
+    for b in braces:
+        res *= 5
+        res += scores[b]
+    return res
 
 
 def solvePartB(data):
     data = _parse_data(data)
-    return
+    data = _remove_corrupted(data)
+
+    openers = ["(", "[", "{", "<"]
+    res = []
+    for line in data:
+        bracestack = []
+        for char in line:
+            if char in openers:
+                bracestack.append(char)
+            else:
+                bracestack.pop()
+        res.append(_score("".join(bracestack[::-1])))
+    return median(sorted(res))
 
 
 if __name__ == "__main__":
-    test = """"""
+    test = """[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]"""
 
-    print(solvePartA(test))
+    print(solvePartB(test))
