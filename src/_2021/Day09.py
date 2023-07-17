e4324d9169a279492fbb1c256247ac2841e14a03
M.W <105752803+troubledprogrammer@users.noreply.github.com>
2023-07-17 21:02:46 +0100

    Solutions to 2021 day 9

--- src/_2021/Day09.py
+++ src/_2021/Day09.py
@@ -5,17 +5,56 @@ def _parse_data(data):
         for c in line:
             l.append(int(c))
         res.append(l)
-    return data
+    return res
 
 
 def solvePartA(data):
     data = _parse_data(data)
-    return
+    size_r = len(data)
+    size_c = len(data[0])
+    res = 0
+    for ri, row in enumerate(data):
+        for ci, cell in enumerate(row):
+            minimum = True
+            for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                rind = ri + dr
+                cind = ci + dc
+                if 0 <= rind < size_r and 0 <= cind < size_c:
+                    if data[rind][cind] <= cell:
+                        minimum = False
+                        break
+            res += 1 + cell if minimum else 0
+    return res
+
+
+def _simulate(data, r, c, size_r, size_c):
+    ri = r
+    ci = c
+    while True:
+        minimum = True
+        for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            rind = ri + dr
+            cind = ci + dc
+            if 0 <= rind < size_r and 0 <= cind < size_c:
+                if data[rind][cind] < data[ri][ci]:
+                    minimum = False
+                    ri, ci = rind, cind
+                    break
+        if minimum: return rind, cind
 
 
 def solvePartB(data):
     data = _parse_data(data)
-    return
+    areas = {}
+    size_r = len(data)
+    size_c = len(data[0])
+    for ri, row in enumerate(data):
+        for ci, cell in enumerate(row):
+            if cell != 9:
+                res = _simulate(data, ri, ci, size_r, size_c)
+                areas[res] = areas.get(res, 0) + 1
+    topthree = sorted([item for _, item in areas.items()], reverse=True)[:3]
+    return topthree[0] * topthree[1] * topthree[2]
 
 
 if __name__ == "__main__":
@@ -25,4 +64,4 @@ if __name__ == "__main__":
 8767896789
 9899965678"""
 
-    print(solvePartA(test))
+    print(solvePartB(test))
